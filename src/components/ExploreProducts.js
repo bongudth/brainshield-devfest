4e8e3a83ebb9d4c3312cfcb6b46f601814b7dc17
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2021-10-24 12:12:29 +0700

    :lipstick: update explore products ui

--- src/components/ExploreProducts.js
+++ src/components/ExploreProducts.js
@@ -1,70 +1,153 @@
 import React from 'react'
-import { List, Avatar, Space } from 'antd';
-import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
-
-const listData = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://ant.design',
-    title: `ant design part ${i}`,
-    avatar: 'https://joeschmoe.io/api/v1/random',
-    description:
-      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
-
-const IconText = ({ icon, text }) => (
-  <Space>
-    {React.createElement(icon)}
-    {text}
-  </Space>
-);
+import { Card, Avatar } from 'antd'
+import { HeartOutlined, CommentOutlined, MoreOutlined } from '@ant-design/icons'
 
 const ExploreProducts = () => {
+  const { Meta } = Card
+
   return (
-    <List
-      style={{ margin: "2rem 20rem" }}
-      itemLayout="vertical"
-      size="large"
-      pagination={{
-        onChange: page => {
-          console.log(page);
-        },
-        pageSize: 3,
-      }}
-      dataSource={listData}
-      footer={
-        <div>
-          <b>ant design</b> footer part
-        </div>
-      }
-      renderItem={item => (
-        <List.Item
-          key={item.title}
-          actions={[
-            <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-            <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-            <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
-          ]}
-          extra={
-            <img
-              width={272}
-              alt="logo"
-              src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-            />
-          }
-        >
-          <List.Item.Meta
-            avatar={<Avatar src={item.avatar} />}
-            title={<a href={item.href}>{item.title}</a>}
-            description={item.description}
+    <div style={{ margin: "2rem 8rem", display: "flex", flexWrap: "wrap" }}>
+      <Card
+        style={{ margin: "0.4rem" }}
+        cover={
+          <img
+            alt="example"
+            src="https://picsum.photos/300/250"
           />
-          {item.content}
-        </List.Item>
-      )}
-    />
+        }
+        actions={[
+          <HeartOutlined key="heart" />,
+          <CommentOutlined key="comment" />,
+          <MoreOutlined key="more" />,
+        ]}
+      >
+        <Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title="Card title"
+          description="This is the description"
+        />
+      </Card>
+      <Card
+        style={{ margin: "0.4rem" }}
+        cover={
+          <img
+            alt="example"
+            src="https://picsum.photos/300/250"
+          />
+        }
+        actions={[
+          <HeartOutlined key="heart" />,
+          <CommentOutlined key="comment" />,
+          <MoreOutlined key="more" />,
+        ]}
+      >
+        <Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title="Card title"
+          description="This is the description"
+        />
+      </Card>
+      <Card
+        style={{ margin: "0.4rem" }}
+        cover={
+          <img
+            alt="example"
+            src="https://picsum.photos/300/250"
+          />
+        }
+        actions={[
+          <HeartOutlined key="heart" />,
+          <CommentOutlined key="comment" />,
+          <MoreOutlined key="more" />,
+        ]}
+      >
+        <Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title="Card title"
+          description="This is the description"
+        />
+      </Card>
+      <Card
+        style={{ margin: "0.4rem" }}
+        cover={
+          <img
+            alt="example"
+            src="https://picsum.photos/300/250"
+          />
+        }
+        actions={[
+          <HeartOutlined key="heart" />,
+          <CommentOutlined key="comment" />,
+          <MoreOutlined key="more" />,
+        ]}
+      >
+        <Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title="Card title"
+          description="This is the description"
+        />
+      </Card>
+      <Card
+        style={{ margin: "0.4rem" }}
+        cover={
+          <img
+            alt="example"
+            src="https://picsum.photos/300/250"
+          />
+        }
+        actions={[
+          <HeartOutlined key="heart" />,
+          <CommentOutlined key="comment" />,
+          <MoreOutlined key="more" />,
+        ]}
+      >
+        <Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title="Card title"
+          description="This is the description"
+        />
+      </Card>
+      <Card
+        style={{ margin: "0.4rem" }}
+        cover={
+          <img
+            alt="example"
+            src="https://picsum.photos/300/250"
+          />
+        }
+        actions={[
+          <HeartOutlined key="heart" />,
+          <CommentOutlined key="comment" />,
+          <MoreOutlined key="more" />,
+        ]}
+      >
+        <Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title="Card title"
+          description="This is the description"
+        />
+      </Card>
+      <Card
+        style={{ margin: "0.4rem" }}
+        cover={
+          <img
+            alt="example"
+            src="https://picsum.photos/300/250"
+          />
+        }
+        actions={[
+          <HeartOutlined key="heart" />,
+          <CommentOutlined key="comment" />,
+          <MoreOutlined key="more" />,
+        ]}
+      >
+        <Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title="Card title"
+          description="This is the description"
+        />
+      </Card>
+    </div>
   )
 }
 
