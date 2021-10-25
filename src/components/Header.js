895ace33cba113b9fc18e2be7d40d38c1a9a6af3
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2021-10-25 10:48:52 +0700

    :heavy_minus_sign: remove google login

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,22 +3,6 @@ import "../assets/scss/Header.scss"
 import { Link } from "react-router-dom"
 import logo from "../assets/images/logo.jpg"
 import { Button } from 'antd'
-import GoogleLogin from 'react-google-login'
-
-const handleLogin = async googleData => {
-  const res = await fetch("http://localhost:8080/api/v1/auth/google", {
-    method: "POST",
-    body: JSON.stringify({
-      token: googleData.tokenId
-    }),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-  const data = await res.json()
-  console.log(data)
-  console.log(googleData)
-}
 
 const Header = () => {
   return (
@@ -29,18 +13,9 @@ const Header = () => {
           <div className="brainshield">Brainshield</div>
         </div>
       </Link>
-      <div>
-        <Link to="/new-product">
-          <Button className="new-product-button">New product</Button>
-        </Link>
-        <GoogleLogin
-          clientId="747812372260-8grkkgva5v847ksiag5vuo9gdvknuu7g.apps.googleusercontent.com"
-          buttonText="Log in with Google"
-          onSuccess={handleLogin}
-          onFailure={handleLogin}
-          cookiePolicy={'single_host_origin'}
-        />
-      </div>
+      <Link to="/new-product">
+        <Button className="new-product-button">New product</Button>
+      </Link>
     </div>
   )
 }
