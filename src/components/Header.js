bd41d79681b246e2c6d96b538c506d22746f33f4
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2021-10-25 15:25:48 +0700

    :sparkles: create metamask button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,29 @@ import React from "react"
 import "../assets/scss/Header.scss"
 import { Link } from "react-router-dom"
 import logo from "../assets/images/logo.png"
+import metamask from "../assets/images/metamask.png"
 import { Button } from 'antd'
 
 const Header = () => {
   return (
     <div className="header-container">
       <Link to="/">
-        <div className="branch-container">
+        <div className="header-left">
           <img src={logo} className="logo" alt="logo"></img>
           <div className="brainshield">Brainshield</div>
         </div>
       </Link>
-      <div>
+      <div className="header-right">
         <Link to="/explore">
           <Button className="button">Explore products</Button>
         </Link>
         <Link to="/new">
           <Button className="button">New product</Button>
+        </Link>
+        <Link to="/">
+          <Button className="button metamask">
+            <img src={metamask} alt="metamask" />
+          </Button>
         </Link>
       </div>
     </div>
