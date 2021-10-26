4f7cd31acc2514de6966cdf8a7c9e7404a6abcc4
derektruong <minhduc0589@gmail.com>
2021-10-26 11:52:50 +0700

    intergrated network config

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,24 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic =
+    "ball once void parent unhappy jump wool group magic assist total estate boy wash panther";
+
 module.exports = {
     networks: {
         development: {
             host: "127.0.0.1",
             port: 7545,
             network_id: "*", // Match any network id
+        },
+        networks: {
+            ropsten: {
+                provider: function () {
+                    return new HDWalletProvider(
+                        mnemonic,
+                        "https://ropsten.infura.io/v3/241f03331919423cba6145c7a2bcc61a"
+                    );
+                },
+                network_id: 3,
+            },
         },
     },
     solc: {
