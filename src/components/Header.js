d0f3097f92267ad43d1850f270a65315e0235f93
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2021-10-26 11:25:38 +0700

    :package: create account modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,35 @@
-import React from "react"
+/* eslint-disable no-template-curly-in-string */
+import React, { useState } from "react"
 import "../assets/scss/Header.scss"
 import { Link } from "react-router-dom"
 import logo from "../assets/images/logo.png"
 import metamask from "../assets/images/metamask.png"
-import { Button, Badge, Card } from 'antd'
+import Identicon from "./Identicon"
+import { Button, Badge, Card, Modal } from 'antd'
+import { CopyOutlined, FolderViewOutlined } from '@ant-design/icons'
 import { useEthers, useEtherBalance } from "@usedapp/core"
 import { formatEther } from "@ethersproject/units"
 
 const Header = () => {
-  const { activateBrowserWallet, account } = useEthers()
+  const { activateBrowserWallet, account, deactivate } = useEthers()
   const etherBalance = useEtherBalance(account)
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
 
   function handleConnectWallet() {
-    activateBrowserWallet();
+    activateBrowserWallet()
+  }
+
+  function handleDeactivateAccount() {
+    deactivate()
+    setIsModalVisible(false)
   }
 
   return (
@@ -24,6 +41,43 @@ const Header = () => {
         </div>
       </Link>
       <div className="header-right">
+        <Modal
+          title="Account"
+          width={300}
+          visible={isModalVisible}
+          onCancel={handleCancel}
+          footer={null}
+          className="modal-container"
+        >
+          <div className="modal-top">
+            <div>Connected with MetaMask</div>
+            <Button
+              className="button"
+              onClick={handleDeactivateAccount}
+            >
+              Change
+            </Button>
+          </div>
+          <div className="modal-content">
+            <Identicon />
+            <div className="account">
+              {account &&
+                `${account.slice(0, 6)}...${account.slice(
+                  account.length - 4,
+                  account.length
+                )}`}
+            </div>
+          </div>
+          <div className="modal-button">
+            <Button icon={<CopyOutlined />} className="button">Copy address</Button>
+            <a href='https://ropsten.etherscan.io/address/${account}'>
+              <Button icon={<FolderViewOutlined />} className="button">View on Explorer</Button>
+            </a>
+          </div>
+          <div className="modal-bottom">
+            <div>Your transactions willl appear here...</div>
+          </div>
+        </Modal>
         <Link to="/explore">
           <Button className="button">Explore products</Button>
         </Link>
@@ -31,17 +85,23 @@ const Header = () => {
           <Button className="button">New product</Button>
         </Link>
         {account ? (
-          <Badge.Ribbon text={etherBalance && parseFloat(formatEther(etherBalance)).toFixed(3) + "ETH"} color="#395266">
-            <Card size="small" className="account-container">
-              <div className="account">
-                {account &&
-                  `${account.slice(0, 6)}...${account.slice(
-                    account.length - 4,
-                    account.length
-                  )}`}
-              </div>
-            </Card>
-          </Badge.Ribbon>
+          <div onClick={showModal} className="account-button">
+            <Badge.Ribbon
+              text={etherBalance && parseFloat(formatEther(etherBalance)).toFixed(3) + "ETH"}
+              color="#395266"
+            >
+              <Card size="small" className="account-container">
+                <Identicon />
+                <div className="account">
+                  {account &&
+                    `${account.slice(0, 6)}...${account.slice(
+                      account.length - 4,
+                      account.length
+                    )}`}
+                </div>
+              </Card>
+            </Badge.Ribbon>
+          </div>
         ) : (
           <Button className="button metamask" onClick={handleConnectWallet}>
             <img src={metamask} alt="metamask" />
