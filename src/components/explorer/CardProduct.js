f68311df5512eae8edfddf22416e5875c045f000
derektruong <minhduc0589@gmail.com>
2021-10-28 00:06:55 +0700

    interacted with blockchain get mine blockchain, handle vote successfull

--- src/components/explorer/CardProduct.js
+++ src/components/explorer/CardProduct.js
@@ -1,27 +1,51 @@
-import React from "react";
-import { useEthers } from "@usedapp/core";
+import React, {useState} from "react";
+import { useEthers, useSendTransaction } from "@usedapp/core";
 import { useAssetsCall, useContractMethod } from "../../hooks/index";
 import "../../assets/scss/ExploreProducts.scss";
-import { Card, Avatar } from "antd";
+import { Card, Avatar, Modal, Button } from "antd";
 import { HeartTwoTone, CoffeeOutlined } from "@ant-design/icons";
+import { utils } from "ethers";
 
 const CartProduct = (props) => {
     const { Meta } = Card;
     const { account } = useEthers();
+	const [isModalVisible, setIsModalVisible] = useState(false);
 
+	
     const [id, accountAddress, ipfsHash, name, description, vote] =
-        useAssetsCall(props.id);
-
+	useAssetsCall(props.id);
+	
     const { state: votePictureState, send: votePicture } =
-        useContractMethod("votePicture");
-
+	useContractMethod("votePicture");
+	
     const clickVoteHandler = () => {
-        if (accountAddress === account) {
-            return;
+		if (accountAddress === account) {
+			return;
         }
         votePicture(props.id);
     };
-
+	
+	const {sendTransaction, state} = useSendTransaction({transactionName: "Send Ethereum"});
+	const clickDonateHandler = () => {
+		if (accountAddress === account) {
+			return;
+		}
+		
+		sendTransaction({to: accountAddress, value: utils.parseEther("0.00015")});
+	};
+	
+	const showModal = () => {
+	  setIsModalVisible(true);
+	};
+  
+	const handleOk = () => {
+	  setIsModalVisible(false);
+	};
+  
+	const handleCancel = () => {
+	  setIsModalVisible(false);
+	};
+	
     return (
         <Card
             key={id}
@@ -38,7 +62,7 @@ const CartProduct = (props) => {
                     <HeartTwoTone twoToneColor="#eb2f96" key="heart" />{" "}
                     <span>{vote ? vote.toNumber() : 0}</span>
                 </span>,
-                <span>
+                <span onClick={clickDonateHandler}>
                     <CoffeeOutlined key="coffee" />
                 </span>,
                 // <CrownOutlined key="crown" />,
